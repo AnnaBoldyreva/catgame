d34b59095960c34dff0d08b715f5a80f75244410
AnnaBoldyreva <annboldyrevaa@gmail.com>
2019-11-13 15:41:08 +0300

    create elements instead <div>

--- src/App.js
+++ src/App.js
@@ -10,25 +10,18 @@ class App extends React.Component {
     currentActiveId: 2
   };
   render() {
+
+    let divs = [1,2,3,4,5,6,7,8,9];
+    let elm = divs.map((number) => <div className="item"><img src='https://www.nationalgeographic.com/content/dam/animals/thumbs/rights-exempt/mammals/d/domestic-dog_thumb.jpg' className={this.state.currentActiveId === number ? 'photo show' : 'photo'}/></div> )
+
     return (
        <div className='App'>
          <div className="wrapper">
-           <div className="item"><img src='https://www.nationalgeographic.com/content/dam/animals/thumbs/rights-exempt/mammals/d/domestic-dog_thumb.jpg' className="photo show"/></div>
-           <div className="item"><img src='https://www.nationalgeographic.com/content/dam/animals/thumbs/rights-exempt/mammals/d/domestic-dog_thumb.jpg' className="photo"/></div>
-           <div className="item"><img src='https://www.nationalgeographic.com/content/dam/animals/thumbs/rights-exempt/mammals/d/domestic-dog_thumb.jpg' className="photo"/></div>
-           <div className="item"><img src='https://www.nationalgeographic.com/content/dam/animals/thumbs/rights-exempt/mammals/d/domestic-dog_thumb.jpg' className="photo"/></div>
-           <div className="item"><img src='https://www.nationalgeographic.com/content/dam/animals/thumbs/rights-exempt/mammals/d/domestic-dog_thumb.jpg' className="photo"/></div>
-           <div className="item"><img src='https://www.nationalgeographic.com/content/dam/animals/thumbs/rights-exempt/mammals/d/domestic-dog_thumb.jpg' className="photo"/></div>
-           <div className="item"><img src='https://www.nationalgeographic.com/content/dam/animals/thumbs/rights-exempt/mammals/d/domestic-dog_thumb.jpg' className="photo"/></div>
-           <div className="item"><img src='https://www.nationalgeographic.com/content/dam/animals/thumbs/rights-exempt/mammals/d/domestic-dog_thumb.jpg' className="photo"/></div>
-           <div className="item"><img src='https://www.nationalgeographic.com/content/dam/animals/thumbs/rights-exempt/mammals/d/domestic-dog_thumb.jpg' className="photo"/></div>
-
-
-
+           {elm}
          </div>
 
          <div className="counter">
-           0
+           {this.state.counter}
          </div>
        </div>
     )
