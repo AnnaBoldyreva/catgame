f9d56c67a427fd29d48feb1c88d743f6d16f0213
AnnaBoldyreva <annboldyrevaa@gmail.com>
2019-11-13 15:56:15 +0300

    creat methods which change our state

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 import React from 'react';
 import './App.css';
 
-
+function getRandomImageIndex(number) {
+  return Math.floor(Math.random() * (number+1));
+}
 
 
 class App extends React.Component {
   state = {
-    counter: 6,
-    currentActiveId: 2
+    counter: 0,
+    currentActiveId: 8
   };
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setCurrentActiveId(getRandomImageIndex(8))
+    }, 1000)
+  }
+
+  setCurrentActiveId = (id) => {
+    this.setState({currentActiveId: id })
+  };
+
+  increaseCounter = () => {
+    this.setState({counter: this.state.counter + 1 })
+  };
+
   render() {
 
     let divs = [1,2,3,4,5,6,7,8,9];
-    let elm = divs.map((number) => <div className="item"><img src='https://www.nationalgeographic.com/content/dam/animals/thumbs/rights-exempt/mammals/d/domestic-dog_thumb.jpg' className={this.state.currentActiveId === number ? 'photo show' : 'photo'}/></div> )
+    let elm = divs.map((number) => <div  onClick={ this.increaseCounter} className="item"><img src='https://1946.ska.ru/media/images/player/mini/5c88df7b06dc57_26735791.png' className={this.state.currentActiveId === number ? 'photo show' : 'photo'}/></div> )
 
     return (
        <div className='App'>
